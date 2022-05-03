6d1637c51a4a4d7148917c3bcafef7f43efaa82a
Henrik Wingerei <henrikwingerei@gmail.com>
2022-05-03 21:05:15 +0200

    Init

---
+++ src/main/kotlin/no/bekk/clean/CleanArchitectureWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package no.bekk.clean
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitectureWorkshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<CleanArchitectureWorkshopApplication>(*args)
+}
