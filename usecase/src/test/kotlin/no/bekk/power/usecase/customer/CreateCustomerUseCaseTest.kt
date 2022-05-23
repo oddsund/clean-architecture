a54c715ba84bc2da50d0c9644f9f86388aa12d9d
Henrik Wingerei <henrikwingerei@gmail.com>
2022-05-23 16:30:51 +0200

    Create simple test for CreateCustomerUseCase

---
+++ usecase/src/test/kotlin/no/bekk/power/usecase/customer/CreateCustomerUseCaseTest.kt
@@ -0,0 +1,61 @@
+package no.bekk.power.usecase.customer
+
+import no.bekk.power.domain.customer.Customer
+import no.bekk.power.domain.customer.CustomerRepository
+import no.bekk.power.domain.valuetypes.CustomerId
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class CreateCustomerUseCaseTest {
+
+    private val customerRepository = InMemoryCustomerRepository()
+    private val createCustomerUseCase = CreateCustomerUseCase(customerRepository)
+
+    @Test
+    internal fun given_valid_customer_customer_is_created() {
+        val (success, customerId, _) = createCustomerUseCase.create(
+            name = "Test Testesen",
+            customerId = "12345678",
+            country = "Norge"
+        )
+
+        assertThat(success).isTrue
+        assertThat(customerId.value).isEqualTo("12345678")
+
+        assertThat(customerRepository.findByCustomerId(customerId)).isNotNull
+    }
+
+    @Test
+    internal fun given_valid_customer_that_already_exists_customer_is_not_created() {
+        val (_, _, _) = createCustomerUseCase.create(
+            name = "Test Testesen",
+            customerId = "12345678",
+            country = "Norge"
+        )
+
+        val (success, _, message) = createCustomerUseCase.create(
+            name = "Test Testesen",
+            customerId = "12345678",
+            country = "Norge"
+        )
+
+        assertThat(success).isFalse
+        assertThat(message).isEqualTo("Customer with id 12345678 already exists")
+    }
+
+    internal class InMemoryCustomerRepository : CustomerRepository {
+        private val customers: MutableMap<CustomerId, Customer> = mutableMapOf()
+
+        override fun findByCustomerId(id: CustomerId): Customer? {
+            return customers[id]
+        }
+
+        override fun save(customer: Customer) {
+            customers[customer.id] = customer
+        }
+
+        override fun update(customer: Customer) {
+            customers[customer.id] = customer
+        }
+    }
+}
