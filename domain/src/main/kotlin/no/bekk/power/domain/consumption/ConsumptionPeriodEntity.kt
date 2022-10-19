c1bf6a9418cb1e0bef2437443b57e1358ac06f6a
Henrik Wingerei <henrik.wingerei@gmail.com>
2022-10-19 22:25:42 +0200

    Revert "Remote Consumption from domain"
    This reverts commit 682be44b904826057a661600573a6f3e2e6606a9.

---
+++ domain/src/main/kotlin/no/bekk/power/domain/consumption/ConsumptionPeriodEntity.kt
@@ -0,0 +1,12 @@
+package no.bekk.power.domain.consumption
+
+import no.bekk.power.domain.valuetypes.Period
+import no.bekk.power.domain.valuetypes.UnitOfMeasurement
+
+class ConsumptionPeriodEntity constructor(
+    internal val period: Period,
+    internal val value: Double,
+    internal val uom: UnitOfMeasurement,
+) {
+
+}
