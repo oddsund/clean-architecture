52920e921b57b11975ef2f77aab67346957ad6e0
Henrik Wingerei <henrik.wingerei@gmail.com>
2022-10-20 14:47:58 +0200

    Remove limit on customerId

--- domain/src/main/kotlin/no/bekk/power/domain/valuetypes/CustomerId.kt
+++ domain/src/main/kotlin/no/bekk/power/domain/valuetypes/CustomerId.kt
@@ -1,8 +1,4 @@
 package no.bekk.power.domain.valuetypes
 
 data class CustomerId(val value: String) {
-
-    init {
-        require(value.length >= 8) { "Invalid length for customerId ${value.length}" }
-    }
 }
